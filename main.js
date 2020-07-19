f8789ac928e4316bd7f8cc361b67ee0519eb04b4
roger87 <willy32136@gmail.com>
2020-07-19 12:23:12 +0800

    連結成功

--- main.js
+++ main.js
@@ -3,6 +3,16 @@ console.log("我是js")
 var vue1 = new Vue({
     el: "#vue1",
     data: {
-        text:"哈囉~我是高金say"
+        text:"哈囉~我喜高金say",
+        a:999,
+        b:3
+    }
+});
+
+var vue2 = new Vue({
+    el: "#vue2",
+    data:{
+        aligment:"center",
+        google:"http://google.com"
     }
 });
